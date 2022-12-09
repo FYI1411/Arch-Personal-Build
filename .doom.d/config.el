bd5c7d2fdb6248e6f7cb3976b56b3f742ea4cc80
pergon <lydieplam@gmail.com>
2022-12-09 13:08:03 +0700

    tabs 4 spaces

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -81,3 +81,7 @@
 (setq key-chord-two-keys-delay 0.5)
 (key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
 (key-chord-mode 1)
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
