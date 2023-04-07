11cf1938d718f181570484c78b71005b5a5b34c7
pergon <lydieplam@gmail.com>
2023-04-07 15:56:11 +0700

    shift arrow to change windows in emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -63,6 +63,7 @@
 (setq key-chord-two-keys-delay 0.5)
 (key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
 (key-chord-mode 1)
+(windmove-default-keybindings)
 
 ;; Splash screen
 (use-package dashboard
