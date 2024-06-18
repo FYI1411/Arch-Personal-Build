e7d2ef9fcafce6682d0892fb584f66e5dfc4b026
fyi1411 <lydieplam@gmail.com>
2024-06-19 02:32:57 +0700

    shift + tab maps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,6 +11,11 @@ vim.g.mapleader = " "
 vim.keymap.set('i', 'jj', '<Esc>')
 vim.keymap.set('n', '<leader>s', ':w<CR>')
 vim.keymap.set('n', '<leader>q', ':qa!<CR>')
+vim.keymap.set("n", "<Tab>", ">>",  opts)
+vim.keymap.set("n", "<S-Tab>", "<<",  opts)
+vim.keymap.set("v", "<Tab>", ">gv", opts)
+vim.keymap.set("v", "<S-Tab>", "<gv", opts)
+vim.keymap.set("i", "<S-Tab>", "<BS>", opts)
 --load lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
