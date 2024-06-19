94bd562a2b38ca1aac9937dd8c83128e5d2653d7
fyi1411 <lydieplam@gmail.com>
2024-06-19 12:38:19 +0700

    neovim formatters

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -3,32 +3,48 @@ return {
 		"williamboman/mason.nvim",
 		config = function()
 			require("mason").setup()
-		end
+		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = {"lua_ls", "bashls", "pyright", "clangd" }
+				ensure_installed = { "lua_ls", "bashls", "pyright", "clangd" },
 			})
-		end
+		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({
-			  settings = {
-				Lua = {
-					diagnostics = {
-						globals = {'vim'},
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
 					},
-				}
-			  }
+				},
 			})
 			lspconfig.bashls.setup({})
 			lspconfig.pyright.setup({})
 			lspconfig.clangd.setup({})
-		end
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			require("mason-null-ls").setup({
+				ensure_installed = { "stylua", "black" },
+			})
+		end,
 	},
 }
