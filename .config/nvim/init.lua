604a3807c68800307c291e62e67b9825fba89faa
fyi1411 <lydieplam@gmail.com>
2024-06-19 14:36:03 +0700

    reformat

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,15 @@
 --options
 vim.opt.number = true
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.expandtab = true
 --maps
 vim.g.mapleader = " "
-vim.keymap.set('i', 'jj', '<Esc>')
-vim.keymap.set('n', '<leader>s', ':w<CR>')
-vim.keymap.set('n', '<leader>q', ':qa!<CR>')
+vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("n", "<leader>s", ":w<CR>")
+vim.keymap.set("n", "<leader>q", ":qa!<CR>")
 vim.keymap.set("n", "<Tab>", ">>")
 vim.keymap.set("n", "<S-Tab>", "<<")
 vim.keymap.set("v", "<Tab>", ">gv")
@@ -18,14 +18,14 @@ vim.keymap.set("i", "<S-Tab>", "<BS>")
 --load lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
