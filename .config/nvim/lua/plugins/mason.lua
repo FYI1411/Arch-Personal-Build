2670db43998a1a8f54eca376ee579958a74381e1
fyi1411 <lydieplam@gmail.com>
2024-06-19 13:10:05 +0700

    autocompletion

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -15,8 +15,10 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+		lazy = false,
 		config = function()
 			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			lspconfig.lua_ls.setup({
 				settings = {
 					Lua = {
@@ -26,9 +28,15 @@ return {
 					},
 				},
 			})
-			lspconfig.bashls.setup({})
-			lspconfig.pyright.setup({})
-			lspconfig.clangd.setup({})
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
